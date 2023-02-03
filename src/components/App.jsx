6c46013ef7b8ab096cea818c15687873ba64f50e
Andrii Popov <popov.andrey80@gmail.com>
2023-02-03 22:23:24 +0200

    add Rates page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Converter />} />
-          <Route path="/rates" element={<Rates />} />
+          <Route path="rates" element={<Rates />} />
         </Route>
       </Routes>
     </>
--- src/pages/rates/Rates.jsx
+++ src/pages/rates/Rates.jsx
@@ -1,5 +1,28 @@
+import { currentExchange } from 'Service/api';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const Rates = () => {
-  return <div>Rates</div>;
+  const [exchange, setExchange] = useState(null);
+
+  useEffect(() => {
+    currentExchange().then(({ rates }) => setExchange(rates));
+  }, []);
+
+  return (
+    <>
+      <div>Rates</div>
+      {exchange && (
+        <ul>
+          {Object.entries(exchange).map(([key, value]) => (
+            <li key={key}>
+              Currency 1 {key}: {(1 / value).toFixed(2)} UAH
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default Rates;
